0bdf55c181d195a1ebf3299461dac4c92ca39896
Noé Sánchez <noeszc@gmail.com>
2025-01-04 18:21:55 -0600

    feat/select (#1)
    * feat(select): more bits
    
    * feat(select): add a `Select` component & namespace

---
+++ packages/components/src/select/namespace.ts
@@ -0,0 +1,25 @@
+export {
+  SelectRoot as Root,
+  SelectTrigger as Trigger,
+  SelectGroup as Group,
+  SelectItem as Item,
+  SelectValue as Value,
+  SelectLabel as Label,
+  SelectContent as Content,
+  SelectSeparator as Separator,
+  SelectScrollUpButton as ScrollUpButton,
+  SelectScrollDownButton as ScrollDownButton,
+} from "./select";
+
+export type {
+  SelectRootProps as RootProps,
+  SelectTriggerProps as TriggerProps,
+  SelectGroupProps as GroupProps,
+  SelectItemProps as ItemProps,
+  SelectValueProps as ValueProps,
+  SelectLabelProps as LabelProps,
+  SelectContentProps as ContentProps,
+  SelectSeparatorProps as SeparatorProps,
+  SelectScrollUpButtonProps as ScrollUpButtonProps,
+  SelectScrollDownButtonProps as ScrollDownButtonProps,
+} from "./select";
