7dde31002b3f60a60dd6582ca2d7361499666434
Noé Sánchez <noeszc@gmail.com>
2025-01-29 16:05:20 -0600

    feat(ui): add a `Toast` component (#3)

---
+++ packages/components/src/toast/namespace.ts
@@ -0,0 +1,8 @@
+export {
+  ToastRoot as Root,
+  ToastClose as Close,
+  ToastAction as Action,
+  ToastDescription as Description,
+  ToastTitle as Title,
+  ToastViewport as Viewport,
+} from "./toast";
